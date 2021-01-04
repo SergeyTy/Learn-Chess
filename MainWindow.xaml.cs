c361c78d2a7859dc5a2f56febb9d68706ce191a1
Sergey <seregio2002@outlook.com>
2021-01-04 19:42:42 +0700

    Create field

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,44 @@ namespace Learn_Chess
         {
             InitializeComponent();
         }
+
+        private void gen_field_Click(object sender, RoutedEventArgs e)
+        {
+
+            grid.Width = 8 * (40);
+            grid.Height = 8 * (40);
+
+            for (int row = 1; row <= 8; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int column = 1; column <= 8; column++)
+                    {
+                        Rectangle rec = new Rectangle();
+                        rec.Width = 40;
+                        rec.Height = 40;
+                        rec.HorizontalAlignment = HorizontalAlignment.Center;
+                        rec.VerticalAlignment = VerticalAlignment.Center;
+                        if ((column % 2) == 0) rec.Fill = System.Windows.Media.Brushes.Beige;
+                        if (column % 2 != 0) rec.Fill = System.Windows.Media.Brushes.DarkGray;
+                        grid.Children.Add(rec);
+                    }
+                }
+                if (row % 2 != 0)
+                {
+                    for (int column = 1; column <= 8; column++)
+                    {
+                        Rectangle rec = new Rectangle();
+                        rec.Width = 40;
+                        rec.Height = 40;
+                        rec.HorizontalAlignment = HorizontalAlignment.Center;
+                        rec.VerticalAlignment = VerticalAlignment.Center;
+                        if ((column % 2) == 0) rec.Fill = System.Windows.Media.Brushes.DarkGray;
+                        if (column % 2 != 0) rec.Fill = System.Windows.Media.Brushes.Beige;
+                        grid.Children.Add(rec);
+                    }
+                }
+            }
+        }
     }
 }
